98965b54303aecc466c002f8759490d4ef5e52b1
Abdullah <abdullahshafiq153@gmail.com>
2026-08-11 07:21:04 -0700

    Fix Payload admin import map

--- app/(payload)/admin/importMap.ts
+++ app/(payload)/admin/importMap.ts
@@ -1,3 +1,7 @@
-import type { ImportMap } from 'payload'
+import { VercelBlobClientUploadHandler as VercelBlobClientUploadHandler_6f67a3d0c79f00d0a6d934b7f063a5c8 } from '@payloadcms/storage-vercel-blob/client'
 
-export const importMap: ImportMap = {}
+/** @type import('payload').ImportMap */
+export const importMap = {
+  '@payloadcms/storage-vercel-blob/client#VercelBlobClientUploadHandler':
+    VercelBlobClientUploadHandler_6f67a3d0c79f00d0a6d934b7f063a5c8,
+}
